c32d8f13a1bd45a94d10e7627d7dde357de7e2ac
lolmageap <ekxk1234@naver.com>
2024-10-08 17:58:01 +0900

    create test and entity factory

---
+++ src/test/kotlin/cherhy/batch/settlement/entityfactory/ExampleEntityFactory.kt
@@ -0,0 +1,36 @@
+package cherhy.batch.settlement.entityfactory
+
+import cherhy.batch.settlement.Example
+import cherhy.batch.settlement.randomize
+import org.jeasy.random.EasyRandom
+import org.jeasy.random.EasyRandomParameters
+import org.jeasy.random.randomizers.text.StringRandomizer
+import kotlin.random.Random
+
+internal object ExampleEntityFactory {
+    fun generate(
+        id: Long = 0,
+        name: String = "test",
+        age: Int = 20,
+    ) =
+        Example(
+            id = id,
+            name = name,
+            age = age,
+        )
+
+    fun generateRandom(): Example {
+        val parameter = EasyRandomParameters()
+            .excludeField {
+                it.name == "id"
+            }
+            .randomize(Example::name) {
+                StringRandomizer(1, 10, Random.nextLong(1, 100)).randomValue
+            }
+            .randomize(Example::age) {
+                (20..80).random()
+            }
+
+        return EasyRandom(parameter).nextObject(Example::class.java)
+    }
+}
