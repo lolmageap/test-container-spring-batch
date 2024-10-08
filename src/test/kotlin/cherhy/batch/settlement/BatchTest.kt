573bddc35e2d5cd34301997a05a8bfc8d4937eea
lolmageap <ekxk1234@naver.com>
2024-10-08 23:48:31 +0900

    add test case

--- src/test/kotlin/cherhy/batch/settlement/BatchTest.kt
+++ src/test/kotlin/cherhy/batch/settlement/BatchTest.kt
@@ -2,17 +2,27 @@ package cherhy.batch.settlement
 
 import cherhy.batch.settlement.TestContainers.postgresContainer
 import cherhy.batch.settlement.entityfactory.ExampleEntityFactory
+import cherhy.batch.settlement.lib.mapParallel
+import com.ninjasquad.springmockk.MockkBean
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.extensions.testcontainers.perSpec
+import io.mockk.verify
+import org.springframework.batch.core.JobExecution
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import kotlin.time.measureTimedValue
 
-class BatchTest : StringSpec({
+@SpringBootTest
+class BatchTest(
+    @Autowired private val jobConfiguration: JobConfiguration,
+    @MockkBean private val exampleJobCompletionNotificationListener: ExampleJobCompletionNotificationListener,
+) : StringSpec({
     beforeTest {
         postgresContainer.start()
         listener(postgresContainer.perSpec())
     }
 
-    "test" {
+    "데이터를 10,000개 생성하고 몇 초 걸리는지 확인한다" {
         val randomExamples =
             measureTimedValue {
                 10_000.mapParallel(ExampleEntityFactory::generateRandom)
@@ -21,5 +31,13 @@ class BatchTest : StringSpec({
             }.value
 
         println("randomExamples: ${randomExamples.size}")
+    }
+
+    "Job을 실행하고 listener가 실행되는지 확인한다" {
+        val job = jobConfiguration.job()
+        job.execute(JobExecution(1))
+
+        verify(exactly = 1) { exampleJobCompletionNotificationListener.beforeJob() }
+        verify(exactly = 1) { exampleJobCompletionNotificationListener.afterJob() }
     }
 })
