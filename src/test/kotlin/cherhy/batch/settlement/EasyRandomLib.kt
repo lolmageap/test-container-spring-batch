ca3d551c8aff225dbe3f5b773a7127be99af957d
lolmageap <ekxk1234@naver.com>
2024-10-08 17:58:57 +0900

    implements random generated functions

---
+++ src/test/kotlin/cherhy/batch/settlement/EasyRandomLib.kt
@@ -0,0 +1,34 @@
+package cherhy.batch.settlement
+
+import org.jeasy.random.EasyRandom
+import org.jeasy.random.EasyRandomParameters
+import org.jeasy.random.api.Randomizer
+import org.springframework.data.mapping.toDotPath
+import java.util.stream.LongStream
+import kotlin.reflect.KProperty
+
+internal inline fun <reified T> T.nextObject(
+    param: EasyRandomParameters = EasyRandomParameters()
+): T {
+    if (!T::class.isData) throw IllegalArgumentException("Only data class is supported")
+    return EasyRandom(param).nextObject(T::class.java)
+}
+
+internal fun <T, R> EasyRandomParameters.randomize(
+    property: KProperty<T>,
+    randomizer: Randomizer<R>,
+): EasyRandomParameters =
+    randomize(
+        { it.name == property.toDotPath() },
+        randomizer,
+    )
+
+internal fun <T> Number.mapParallel(
+    block: () -> T,
+): List<T> =
+    LongStream.range(0, this.toLong())
+        .parallel()
+        .mapToObj {
+            block()
+        }
+        .toList()
