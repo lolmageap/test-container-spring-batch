56692a5172a35d39c70bb84399db01a2109579a5
lolmageap <ekxk1234@naver.com>
2024-10-09 23:07:19 +0900

    fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.spring") version "1.9.20"
-    kotlin("plugin.jpa") version "1.7.22"
 }
 
 group = "kotlin.batch"
@@ -28,24 +27,26 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging:3.0.5")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-batch")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.batch:spring-batch-core:5.1.2")
+    implementation("org.springframework.batch:spring-batch-test:5.1.2")
 
     testRuntimeOnly("org.postgresql:postgresql")
-    testImplementation("org.testcontainers:postgresql")
+    implementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:jdbc:1.20.2")
+    testImplementation("org.flywaydb:flyway-core:10.15.0")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.jeasy:easy-random-core:5.0.0")
     testImplementation("com.ninja-squad:springmockk:4.0.2")
     testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
     testImplementation("io.kotest:kotest-assertions-core:5.7.2")
-    testImplementation("org.springframework.batch:spring-batch-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation("org.flywaydb:flyway-database-postgresql:10.15.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.2")
     testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
 }
 
