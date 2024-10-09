a29ac47451c0d2dd69990c5ebde586c3d24500c3
lolmageap <ekxk1234@naver.com>
2024-10-09 23:08:55 +0900

    add driver property

--- src/main/kotlin/cherhy/batch/settlement/DataSourceProperty.kt
+++ src/main/kotlin/cherhy/batch/settlement/DataSourceProperty.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "spring.datasource")
 data class DataSourceProperty(
+    val driverClassName: String,
     val url: String,
     val username: String,
     val password: String,
