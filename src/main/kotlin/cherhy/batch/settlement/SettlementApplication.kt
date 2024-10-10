b50aec6c89e5e62d922aadba72c1e9901a80696b
lolmageap <ekxk1234@naver.com>
2024-10-10 17:56:09 +0900

    disable FlywayAutoConfiguration

--- src/main/kotlin/cherhy/batch/settlement/SettlementApplication.kt
+++ src/main/kotlin/cherhy/batch/settlement/SettlementApplication.kt
@@ -2,11 +2,16 @@ package cherhy.batch.settlement
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@SpringBootApplication(
+    exclude = [
+        DataSourceAutoConfiguration::class,
+        FlywayAutoConfiguration::class,
+    ])
 @EnableBatchProcessing
 @ConfigurationPropertiesScan
 class SettlementApplication
