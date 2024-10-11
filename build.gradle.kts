8a8852d091ead6b942f143b1593e34dca2c03308
lolmageap <ekxk1234@naver.com>
2024-10-11 23:23:02 +0900

    add all open and no arg plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.spring") version "1.9.20"
+    kotlin("plugin.allopen") version "1.9.20"
+    kotlin("plugin.noarg") version "1.9.20"
 }
 
 group = "kotlin.batch"
@@ -22,10 +24,12 @@ repositories {
 
 dependencies {
     runtimeOnly("org.postgresql:postgresql")
+    implementation("org.flywaydb:flyway-core:10.15.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("io.github.microutils:kotlin-logging:3.0.5")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.flywaydb:flyway-database-postgresql:10.15.0")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.batch:spring-batch-core:5.1.2")
     implementation("org.springframework.batch:spring-batch-test:5.1.2")
@@ -33,14 +37,12 @@ dependencies {
     testRuntimeOnly("org.postgresql:postgresql")
     implementation("org.testcontainers:postgresql")
     testImplementation("org.testcontainers:jdbc:1.20.2")
-    testImplementation("org.flywaydb:flyway-core:10.15.0")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.jeasy:easy-random-core:5.0.0")
     testImplementation("com.ninja-squad:springmockk:4.0.2")
     testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
     testImplementation("io.kotest:kotest-assertions-core:5.7.2")
     testImplementation("org.springframework.kafka:spring-kafka-test")
-    testImplementation("org.flywaydb:flyway-database-postgresql:10.15.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
@@ -58,4 +60,12 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+allOpen {
+    annotation("cherhy.batch.settlement.annotation.Table")
+}
+
+noArg {
+    annotation("cherhy.batch.settlement.annotation.Table")
 }
