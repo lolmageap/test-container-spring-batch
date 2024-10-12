c95a0946c8a281f7061f2dfbb1a6d34bdce6c8f0
lolmageap <ekxk1234@naver.com>
2024-10-12 23:17:03 +0900

    add file constants

--- src/main/kotlin/cherhy/batch/settlement/util/property/BatchProperties.kt
+++ src/main/kotlin/cherhy/batch/settlement/util/property/BatchProperties.kt
@@ -5,22 +5,31 @@ object BatchProperties {
 
     object Job {
         const val EXAMPLE_JOB = "exampleJob"
+        const val EXAMPLE_FILE_JOB = "exampleFileJob"
     }
 
     object Step {
         const val EXAMPLE_FIRST_STEP = "firstStep"
         const val EXAMPLE_LAST_STEP = "lastStep"
+        const val EXAMPLE_FILE_FIRST_STEP = "exampleFileFirstStep"
+        const val EXAMPLE_FILE_LAST_STEP = "exampleFileLastStep"
     }
 
     object ItemReader {
         const val EXAMPLE_ITEM_READER = "exampleItemReader"
+        const val CSV_FILE_ITEM_READER = "csvFileItemReader"
+        const val TEXT_FILE_ITEM_READER = "textFileItemReader"
     }
 
     object ItemProcessor {
         const val EXAMPLE_ITEM_PROCESSOR = "exampleItemProcessor"
+        const val CSV_FILE_ITEM_PROCESSOR = "csvFileItemProcessor"
+        const val TEXT_FILE_ITEM_PROCESSOR = "textFileItemProcessor"
     }
 
     object ItemWriter {
         const val EXAMPLE_ITEM_WRITER = "exampleItemWriter"
+        const val CSV_FILE_ITEM_WRITER = "csvFileItemWriter"
+        const val TEXT_FILE_ITEM_WRITER = "textFileItemWriter"
     }
 }
---
+++ src/main/kotlin/cherhy/batch/settlement/util/property/FilePath.kt
@@ -0,0 +1,9 @@
+package cherhy.batch.settlement.util.property
+
+object FilePath {
+    private const val FILE_PATH = "files/"
+    const val INPUT_TEXT_FILE_NAME = "${FILE_PATH}input.txt"
+    const val OUTPUT_TEXT_FILE_NAME = "${FILE_PATH}output.txt"
+    const val INPUT_CSV_FILE_NAME = "${FILE_PATH}input.csv"
+    const val OUTPUT_CSV_FILE_NAME = "${FILE_PATH}output.csv"
+}
