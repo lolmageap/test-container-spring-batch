f9de273a15daa9b00d82af1dc01802959f14f9a8
lolmageap <ekxk1234@naver.com>
2024-10-20 17:48:50 +0900

    add file rollback notification listener

--- src/main/kotlin/cherhy/batch/settlement/file/job/FileJobConfiguration.kt
+++ src/main/kotlin/cherhy/batch/settlement/file/job/FileJobConfiguration.kt
@@ -4,6 +4,7 @@ import cherhy.batch.settlement.file.process.ExampleFieldSetMapper
 import cherhy.batch.settlement.file.process.ExampleFileJobCompletionNotificationListener
 import cherhy.batch.settlement.jdbc.example.process.ExampleItemProcessor.Companion.DISCOUNT_PRICE
 import cherhy.batch.settlement.model.Example
+import cherhy.batch.settlement.util.ExampleTempFileGenerator
 import cherhy.batch.settlement.util.property.BatchProperties.CHUNK_SIZE
 import cherhy.batch.settlement.util.property.BatchProperties.ItemProcessor.CSV_FILE_ITEM_PROCESSOR
 import cherhy.batch.settlement.util.property.BatchProperties.ItemReader.CSV_FILE_ITEM_READER
@@ -15,6 +16,9 @@ import cherhy.batch.settlement.util.property.BatchProperties.Step.EXAMPLE_FILE_L
 import cherhy.batch.settlement.util.property.FilePath.INPUT_CSV_FILE_NAME
 import cherhy.batch.settlement.util.property.FilePath.OUTPUT_CSV_FILE_NAME
 import cherhy.batch.settlement.util.property.FilePath.OUTPUT_TEXT_FILE_NAME
+import org.springframework.batch.core.BatchStatus
+import org.springframework.batch.core.JobExecution
+import org.springframework.batch.core.JobExecutionListener
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
@@ -48,28 +52,33 @@ class FileJobConfiguration(
             .start(fileFirstStep())
             .next(fileLastStep())
             .listener(exampleFileJobCompletionNotificationListener)
+            .listener(rollbackNotificationListener())
             .build()
 
     @Bean(EXAMPLE_FILE_FIRST_STEP)
-    fun fileFirstStep() = StepBuilder(
-        EXAMPLE_FILE_FIRST_STEP,
-        jobRepository,
-    ).chunk<Example, Example>(CHUNK_SIZE, transactionManager)
-        .reader(csvFileItemReader())
-        .processor(csvFileItemProcessor())
-        .writer(csvFileItemWriter())
-        .writer(textFileItemWriter())
-        .build()
+    fun fileFirstStep() =
+        StepBuilder(
+            EXAMPLE_FILE_FIRST_STEP,
+            jobRepository,
+        ).chunk<Example, Example>(CHUNK_SIZE, transactionManager)
+            .reader(csvFileItemReader())
+            .processor(csvFileItemProcessor())
+            .writer(csvFileItemWriter())
+            .writer(textFileItemWriter())
+            .build()
 
     @Bean(EXAMPLE_FILE_LAST_STEP)
-    fun fileLastStep() = StepBuilder(
-        EXAMPLE_FILE_LAST_STEP,
-        jobRepository,
-    ).chunk<Example, Example>(CHUNK_SIZE, transactionManager)
-        .reader(csvFileItemReader())
-        .processor(csvFileItemProcessor())
-        .writer(csvFileItemWriter())
-        .build()
+    fun fileLastStep() =
+        StepBuilder(
+            EXAMPLE_FILE_LAST_STEP,
+            jobRepository,
+        ).chunk<Example, Example>(CHUNK_SIZE, transactionManager)
+            .reader(csvFileItemReader())
+            .faultTolerant()
+            .skip(Exception::class.java)
+            .processor(csvFileItemProcessor())
+            .writer(csvFileItemWriter())
+            .build()
 
     @Bean(CSV_FILE_ITEM_READER)
     fun csvFileItemReader(): ItemReader<Example> {
@@ -111,12 +120,35 @@ class FileJobConfiguration(
     }
 
     @Bean(TEXT_FILE_ITEM_WRITER)
-    fun textFileItemWriter(): ItemWriter<Example> {
-        return FlatFileItemWriter<Example>().apply {
+    fun textFileItemWriter() =
+        FlatFileItemWriter<Example>().apply {
             setResource(outputTextFileResource)
             setLineAggregator { item ->
                 with(item) { "${id}: ${name}는 ${price}원으로 값이 변경 되었습니다." }
             }
         }
-    }
+
+    @Bean
+    fun rollbackNotificationListener() =
+        object : JobExecutionListener {
+            override fun beforeJob(
+                jobExecution: JobExecution,
+            ) {
+                ExampleTempFileGenerator.createTempFile(jobExecution.jobId)
+            }
+
+            override fun afterJob(
+                jobExecution: JobExecution,
+            ) {
+                if (jobExecution.status == BatchStatus.COMPLETED) {
+                    ExampleTempFileGenerator.deleteTempFile(jobExecution.jobId)
+                }
+
+                if (jobExecution.status == BatchStatus.FAILED) {
+                    val tempFile = ExampleTempFileGenerator.getTempFile(jobExecution.jobId)
+                    inputCsvFileResource.file.copyTo(tempFile, overwrite = true)
+                    throw RuntimeException("Job failed")
+                }
+            }
+        }
 }
